6d1ebd5e1c5c5899465d091219f6e036f4f9b551
SimaxSaab <minoreat@gmail.com>
2024-01-12 01:49:05 +0200

    feat: task 2 green and yellow cubes

--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,26 +9,57 @@ using UnityEngine.UI;
 public class CubeManager : MonoBehaviour
 {
     public Button _play;
-    
+    public Button _playCubesCombination;
+
     public GameObject CubePrefab;
+    public GameObject GreenCubePrefab;
+    public GameObject YellowCubePrefab;
+    public GameObject[] cubes;
+
     public int width = 10;
     public int height = 10;
     // Start is called before the first frame update
     void Start()
     {
-        _play.onClick.AddListener(ClickPlay);
+        _play.onClick.AddListener(() => { ClickPlay(1); });
+        _playCubesCombination.onClick.AddListener(() => { ClickPlay(2); });
     }
 
-    void ClickPlay()
+    void ClickPlay(int flag)
     {
+        if (cubes.Length > 0)
+        {
+            foreach (GameObject cube in cubes)
+            {
+                Destroy(cube);
+            }
+        }
+
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
-                CubePrefab.name = "Cube " + (x * width + z);
-                GameObject cube = Instantiate(CubePrefab, new Vector3(x * 1.1f, 0, z * 1.1f), Quaternion.identity);
+                GameObject CubeInst = CubePrefab;
+
+                if (flag == 2)
+                {
+                    float randomValue = Random.value;
+
+                    if (randomValue <= 0.7f)
+                    {
+                        CubeInst = GreenCubePrefab;
+                    }
+                    else
+                    {
+                        CubeInst = YellowCubePrefab;
+                    }
+                }
+                CubeInst.name = "Cube" + (x * width + z);
+                CubeInst.tag = "Cube";
+                GameObject cube = Instantiate(CubeInst, new Vector3(x * 1.1f, 0, z * 1.1f), Quaternion.identity);
             }
         }
+        cubes = GameObject.FindGameObjectsWithTag("Cube");
     }
 
     // Update is called once per frame
